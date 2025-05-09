9a2880775c87b9b16078b40dafb2364da078c2e4
Tomas <1257435+tomasantonj@users.noreply.github.com>
2025-05-09 08:20:32 +0200

    Test tre av fizzbuzz
    Började dela upp logik i olika funktioner, få lite struktur på programmet.

---
+++ thirdproject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzzThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            FizzBuzz(2, 3, 7);
+        }
+
+        private static void FizzBuzz(int x, int y, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % y == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % x == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        
+    }   
+}
